9ee8ee8d8754a8fc607774188af8237beefc04ca
MitraPaideti <120472116+MitraPaideti@users.noreply.github.com>
2023-05-18 14:31:42 +0530

    Success

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-# Provider configuration
-provider "aws" {
-  region = "us-west-2" # Replace with your desired region
-}
-
 # Create a VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16" # Replace with your desired CIDR block
+  cidr_block = "10.0.0.0/16" 
   tags = {
     Name = "my-vpc"
   }
@@ -14,8 +9,8 @@ resource "aws_vpc" "my_vpc" {
 # Create a subnet
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24" # Replace with your desired subnet CIDR block
-  availability_zone       = "us-west-2a" # Replace with your desired availability zone
+  cidr_block              = "10.0.1.0/24" 
+  availability_zone       = "us-west-2a"
   tags = {
     Name = "my-subnet"
   }
