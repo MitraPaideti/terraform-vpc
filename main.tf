18522248583461291c6a9f5b9155c819c0672154
MitraPaideti <120472116+MitraPaideti@users.noreply.github.com>
2023-05-19 12:23:11 +0530

    Success

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "my-vpc" {
+module "vpc" {
   source              = "./vpc"
   vpc_cidr_block      = "10.0.0.0/16"
   subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
