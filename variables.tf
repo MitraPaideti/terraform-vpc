c39a3c9c46498f42336d37fa14e8f7a383a0fb93
MitraPaideti <120472116+MitraPaideti@users.noreply.github.com>
2023-05-19 12:52:56 +0530

    Success

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-west-2"
-}
+#variable "aws_region" {
+  #description = "AWS region"
+  #default     = "us-west-2"
+#}
 
 variable "vpc_cidr_block" {
   description = "VPC CIDR block"
