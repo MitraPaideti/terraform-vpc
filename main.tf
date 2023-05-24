b85c7f09b0d2d829d1c6c85771602da9ea02ce6e
MitraPaideti <120472116+MitraPaideti@users.noreply.github.com>
2023-05-24 11:05:31 +0530

    Success

--- main.tf
+++ main.tf
@@ -4,24 +4,10 @@ module "vpc" {
  /* vpc_name           = "my-vpc-mitra"
   vpc_cidr_block     = "10.0.0.0/16"
   subnet_cidr_block = "10.0.0.0/24"
-  availability_zone = "us-west-2a"
-  
-  vpc_name           =  var.vpc_name
-  vpc_cidr_block      = var.vpc_cidr_block
-  subnet_cidr_block  = var.subnet_cidr_block
-  availability_zone = var.availability_zone*/
-  
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+  availability_zone = "us-west-2a"*/
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_security_group" "my_security_group" {
-  vpc_id = aws_vpc.my_vpc.id
-
-}
+  vpc_name           = var.vpc_name
+  vpc_cidr_block     = var.vpc_cidr_block
+  subnet_cidr_block = var.subnet_cidr_block 
+  availability_zone = var.availability_zone 
 }
