3bb09fa74b072b683e52a5214f19e08c3b4affb3
MitraPaideti <120472116+MitraPaideti@users.noreply.github.com>
2023-05-24 11:09:39 +0530

    Success

--- output.tf
+++ output.tf
@@ -1,11 +1,15 @@
 output "vpc_id" {
-  value = aws_vpc.my_vpc.id
+  value = aws_vpc.vpc_id
 }
 
 output "subnet_id" {
-  value = aws_subnet.my_subnet.id
+  value = aws_subnet.subnet_id
 }
 
 output "security_group_id" {
-  value = aws_security_group.my_security_group.id
-}
+  value = aws_security_group.security_group_id
+  }
+
+  output "subnet_cidr_block" {
+  value = aws_subnet_cidr_block.subnet_cidr_block
+  }
